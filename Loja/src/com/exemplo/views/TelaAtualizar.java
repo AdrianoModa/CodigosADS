b2880031fe14f8ff7ae339ea4304fc7558c2346b
adrianomoda <adrianomodafeitosa@gmail.com>
2017-02-25 14:53:18 -0300

    Adicionando tela Atualizar Cliente

--- Loja/src/com/exemplo/views/TelaAtualizar.java
+++ Loja/src/com/exemplo/views/TelaAtualizar.java
@@ -3,7 +3,6 @@ package com.exemplo.views;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
