aff768d2f17fe56b42eb04e1f0006c8543584a68
adrianomoda <adrianomodafeitosa@gmail.com>
2017-11-07 14:19:55 -0300

    Botao Sair criado com condição para sair.

--- EscreverNoArquivo/src/br/com/adriano/ViewAbrirArquivo.java
+++ EscreverNoArquivo/src/br/com/adriano/ViewAbrirArquivo.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
@@ -48,7 +49,21 @@ public class ViewAbrirArquivo extends JFrame {
 				cla.abrirArquivoNotepad();
 			}
 		});
-		btnEscreverNoArquivo.setBounds(126, 103, 182, 56);
+		btnEscreverNoArquivo.setBounds(126, 50, 182, 56);
 		contentPane.add(btnEscreverNoArquivo);
+		
+		JButton btnSairDoPrograma = new JButton("Sair do Programa");
+		btnSairDoPrograma.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (btnSairDoPrograma.isDisplayable()) {
+					int sim = JOptionPane.showConfirmDialog(null, "Deseja sair do Programa?");
+					if(sim == 0){
+						System.exit(0);
+					}
+				}
+			}
+		});
+		btnSairDoPrograma.setBounds(126, 156, 182, 56);
+		contentPane.add(btnSairDoPrograma);
 	}
 }
